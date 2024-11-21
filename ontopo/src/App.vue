021000ac4a29b90d87fdedefa4f110b79a85c577
Jovan Toskic <jovan@yada.ai>
2024-11-21 12:22:30 +0100

    Add everything else

---
+++ ontopo/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+
+import { RouterView, useRouter } from 'vue-router'
+import { useRestaurantStore } from './stores/restaurantStore'
+
+import 'primeicons/primeicons.css'
+
+const { jwtToken } = useRestaurantStore()
+
+const router = useRouter()
+
+onMounted(() => {
+  //token se gubi kada radim refresh, vidi sta ces sa ovim i kako...
+  // if (!jwtToken.value) {
+  //   router.push('/')
+  // }
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped></style>

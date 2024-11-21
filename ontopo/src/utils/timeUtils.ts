0494216013215f85f0bc21b23855f50d9383812c
Jovan Toskic <jovan@yada.ai>
2024-11-21 12:21:19 +0100

    Add tiem and date utils

---
+++ ontopo/src/utils/timeUtils.ts
@@ -0,0 +1,8 @@
+export const transformTimeToFormat = (dateString: string): string => {
+  const date = new Date(dateString)
+
+  const hours = String(date.getUTCHours()).padStart(2, '0')
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0')
+
+  return `${hours}${minutes}`
+}
